74860d5f9856552af6e1d09eb2ee1784878413fe
Kjarrigan <dnd@kjarrigan.de>
2016-04-15 13:47:34 +0200

    Testphase done -> Reorganize code
    Replace static values with character-variable

--- character_sheet_pdf.rb
+++ character_sheet_pdf.rb
@@ -1,12 +1,18 @@
 require 'prawn'
+require_relative 'sb'
 
 module Helper
+  WIDTH = 598
+  FONT_NORMAL = 9
+  FONT_SMALL = 4
+  FONT_TINY = 2
+
   def text_field(label, content, x, w)
     text_box content, at: [x, cursor]
     stroke_line x, cursor-10, x+w-10, cursor-10
     text_box label, at: [x, cursor-12], size: FONT_SMALL
   end
-  
+
   def black_box(content, x, w, margin: 3, weight: :normal)
     self.fill_color = "000000"
     fill_and_stroke_rectangle [x, cursor+2], w-margin, FONT_NORMAL+2
@@ -14,92 +20,103 @@ module Helper
     text_box content, at: [x,cursor], width: w-margin, align: :center, weight: weight
     self.fill_color = "000000"
   end
-  
+
   def white_box(content, x, w, margin: 3, weight: :normal)
     self.fill_color = "ffffff"
     fill_and_stroke_rectangle [x, cursor+2], w-margin, FONT_NORMAL+2
     self.fill_color = "000000"
-    text_box content, at: [x,cursor], width: w-margin, align: :center, weight: weight    
+    text_box content, at: [x,cursor], width: w-margin, align: :center, weight: weight
   end
-  
+
   def label_box(content, x, w)
     text_box content, at: [x, cursor], width: w, height: FONT_NORMAL+2, size: FONT_SMALL, align: :center, valign: :bottom
   end
 end
 
-WIDTH = 598
-FONT_NORMAL = 9
-FONT_SMALL = 4
-FONT_TINY = 2
+class Stringifier
+  attr_reader :obj
+  def initialize(obj)
+    @obj = obj
+  end
 
-Prawn::Document.include Helper
+  def method_missing(name, &block)
+    resp = if block
+      obj.send(name, &block)
+    else
+      obj.send(name)
+    end
+    resp.to_s
+  end
+end
 
-cs = Prawn::Document.new(margin: 5)
-cs.font_size = FONT_NORMAL
-cs.line_width = 0.5
+class CharacterSheet35 < Prawn::Document
+  include Helper
 
-# Description
-cs.text_field 'CHARACTER NAME', 'Gwyneth Llywelyn', 0, 200
-cs.text_field 'PLAYER', 'Holger', 200, 200
-cs.move_down 20
+  attr_accessor :character
+  def initialize
+    super(margin: 5)
+    font_size = FONT_NORMAL
+    line_width = 0.5
+  end
 
-cs.text_field 'CLASS AND LEVEL', 'Lvl. 3 Sorceress', 0, 200
-cs.text_field 'RACE', 'Human', 200, 67
-cs.text_field 'Alignment', 'Neutral Good', 267, 66
-cs.text_field 'Deity', '-', 333, 67
-cs.move_down 20
+  def basic_information
+    # Description
+    text_field 'CHARACTER NAME', character.name, 0, 200
+    text_field 'PLAYER', character.player, 200, 200
+    move_down 20
 
-cs.text_field 'SIZE', 'Medium', 0, 50
-cs.text_field 'AGE', '18', 50, 50
-cs.text_field 'GENDER', 'Female', 100, 50
-cs.text_field 'HEIGHT', '165 cm', 150, 50
-cs.text_field 'WEIGHT', '52 kg', 200, 50
-cs.text_field 'EYES', 'blue', 250, 50
-cs.text_field 'HAIR', 'black', 300, 50
-cs.text_field 'SKIN', 'pale', 350, 50
-cs.move_down 25
+    text_field 'CLASS AND LEVEL', character.klass, 0, 200
+    text_field 'RACE', character.race, 200, 67
+    text_field 'Alignment', character.alignment, 267, 66
+    text_field 'Deity', character.deity, 333, 67
+    move_down 20
 
-# Abilites
-cs.label_box 'ABILITY NAME', 0, 40
-cs.label_box 'ABILITY SCORE', 40, 30
-cs.label_box 'ABILITY MODIFIER', 70, 30
-cs.label_box 'TEMPORARY SCORE', 100, 30
-cs.label_box 'TEMPORARY MODIFIER', 130, 30
-cs.move_down 14
-cs.black_box 'STR', 0, 40, weight: :bold
-cs.white_box '8', 40, 30
-cs.white_box '-1', 70, 30
-cs.white_box '', 100, 30
-cs.white_box '', 130, 30
-cs.move_down 14
-cs.black_box 'DEX', 0, 40, weight: :bold
-cs.white_box '14', 40, 30
-cs.white_box '+2', 70, 30
-cs.white_box '', 100, 30
-cs.white_box '', 130, 30
-cs.move_down 14
-cs.black_box 'CON', 0, 40, weight: :bold
-cs.white_box '12', 40, 30
-cs.white_box '+1', 70, 30
-cs.white_box '', 100, 30
-cs.white_box '', 130, 30
-cs.move_down 14
-cs.black_box 'INT', 0, 40, weight: :bold
-cs.white_box '14', 40, 30
-cs.white_box '+2', 70, 30
-cs.white_box '', 100, 30
-cs.white_box '', 130, 30
-cs.move_down 14
-cs.black_box 'WIS', 0, 40, weight: :bold
-cs.white_box '10', 40, 30
-cs.white_box '0', 70, 30
-cs.white_box '', 100, 30
-cs.white_box '', 130, 30
-cs.move_down 14
-cs.black_box 'CHA', 0, 40, weight: :bold
-cs.white_box '16', 40, 30
-cs.white_box '+3', 70, 30
-cs.white_box '', 100, 30
-cs.white_box '', 130, 30
+    text_field 'SIZE', character.size, 0, 50
+    text_field 'AGE', character.age, 50, 50
+    text_field 'GENDER', character.gender, 100, 50
+    text_field 'HEIGHT', character.height, 150, 50
+    text_field 'WEIGHT', character.weight, 200, 50
+    text_field 'EYES', character.eyes, 250, 50
+    text_field 'HAIR', character.hair, 300, 50
+    text_field 'SKIN', character.skin, 350, 50
+    move_down 25
+  end
 
-cs.render_file('cs.pdf')
+  def abilites
+    label_box 'ABILITY NAME', 0, 40
+    label_box 'ABILITY SCORE', 40, 30
+    label_box 'ABILITY MODIFIER', 70, 30
+    label_box 'TEMPORARY SCORE', 100, 30
+    label_box 'TEMPORARY MODIFIER', 130, 30
+
+    [:str, :dex, :con, :int, :wis, :cha].each do |ab|
+      move_down 14
+      black_box ab.to_s.upcase, 0, 40, weight: :bold
+      white_box character.send(ab), 40, 30
+      white_box character.send("#{ab}_mod"), 70, 30
+      white_box none_if_blank(character.send("#{ab}_temp")), 100, 30
+      white_box none_if_blank(character.send("#{ab}_temp_mod")), 130, 30
+    end
+  end
+
+  def generate_for(char)
+    @character = Stringifier.new(char)
+    basic_information
+    abilites
+  end
+
+  def save
+    render_file('sheet.pdf')
+  end
+
+  private
+  def none_if_blank(str)
+    (str.nil? or str.empty? or str == '0') ? '' : str
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  cs = CharacterSheet35.new
+  cs.generate_for Gwyn.new
+  cs.save
+end
